ddc8292b191308f3e4e22537fff505d2d1a7c767
Rachel Salois <rachel.salois@flatironschool.com>
2018-12-20 11:09:30 -0500

    lecture code

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,1 +1,35 @@
 import {Jumbler} from '../index';
+
+describe('addLetter', () => {
+
+  const jumbler = new Jumbler()
+
+  it('should take in a letter and return all letters', () => {
+    jumbler.addLetter("a")
+    expect(jumbler.allLetters).toEqual(["a"])
+    expect(jumbler.allLetters.length).toEqual(1)
+  })
+
+  it('should take in another letter and return all letters', () => {
+    jumbler.addLetter("b")
+    expect(jumbler.allLetters).toEqual(["a", "b"])
+    expect(jumbler.allLetters.length).toEqual(2)
+  })
+})
+
+describe('rotate', () => {
+  const jumbler = new Jumbler()
+
+  it('should move the letter in the first position to the last position', () =>{
+    jumbler.allLetters = ["a", "b", "c"]
+    jumbler.rotate()
+    expect(jumbler.allLetters[jumbler.allLetters.length - 1]).toEqual("a")
+    expect(jumbler.allLetters[0]).toEqual("b")
+  })
+
+  xit('should move the letter in the first position to the last position', () =>{
+    jumbler.allLetters = ["d", "e", "f"]
+    jumbler.rotate()
+    expect(jumbler.allLetters[jumbler.allLetters.length - 1]).toEqual("d")
+  })
+})
--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 export class Jumbler {
 
+constructor() {
+  this.allLetters = []
+}
+
+addLetter(letter) {
+  this.allLetters.push(letter)
+}
+
+rotate() {
+  const first = this.allLetters.shift()
+  this.allLetters.push(first)
+}
+
 }
